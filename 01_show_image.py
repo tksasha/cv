eaff039635f39a2f342cc9f6219bb26c24a3c4f9
Oleksandr Tkachuk <tksasha.me@gmail.com>
2025-10-07 09:58:26 +0300

    added show image

---
+++ 01_show_image.py
@@ -0,0 +1,11 @@
+import cv2 as cv
+
+if __name__ == "__main__":
+    image = cv.imread("000-kpi.jpg")
+    assert image is not None, "failed to read image"
+
+    cv.imshow("Show Image", image)
+
+    _ = cv.waitKey(0)
+
+    cv.destroyAllWindows()
